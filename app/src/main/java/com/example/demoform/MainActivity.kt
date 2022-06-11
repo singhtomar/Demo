d6c22a0702e388dc27deeebe68409ddf8771df7d
Mohan singh <mohansinghtomar30@gmail.com>
2022-06-11 17:21:56 +0530

    This is demo form in kotlin

---
+++ app/src/main/java/com/example/demoform/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.demoform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
